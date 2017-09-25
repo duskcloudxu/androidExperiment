a449de49d9ef1f0ba275b350c269846d3622e960
duskcloudxu <cmbjx1@qq.com>
2017-09-25 17:09:57 +0800

    9.21:intent广播调试

--- app/src/main/java/com/example/admin/myapplication/MainActivity.java
+++ app/src/main/java/com/example/admin/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.admin.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,16 +13,16 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main,menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId()==R.id.add_item)
-            Toast.makeText(MainActivity.this,"you clicked Add",Toast.LENGTH_SHORT).show();
-        else{
-            Toast.makeText(MainActivity.this,"you clicked Del",Toast.LENGTH_SHORT).show();
+        if (item.getItemId() == R.id.add_item)
+            Toast.makeText(MainActivity.this, "you clicked Add", Toast.LENGTH_SHORT).show();
+        else {
+            Toast.makeText(MainActivity.this, "you clicked Del", Toast.LENGTH_SHORT).show();
             finish();
         }
         return super.onOptionsItemSelected(item);
@@ -31,18 +32,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btn1=(Button) findViewById(R.id.button);//findViewById返回的是view对象，
-                                                       // 即在layout里出现过的对象,需要把它转型成为button对象
-                                                       //详见向下转型
-        btn1.setOnClickListener(new View.OnClickListener(){  // 这个是写在setOnClickListener底下的类
+        Button btn1 = (Button) findViewById(R.id.button);//findViewById返回的是view对象，
+        // 即在layout里出现过的对象,需要把它转型成为button对象
+        //详见向下转型
+        btn1.setOnClickListener(new View.OnClickListener() {  // 这个是写在setOnClickListener底下的类
             @Override
-            public void onClick(View V)
-            {
-                Toast.makeText(MainActivity.this, getResources().getResourceName(V.getId()), Toast.LENGTH_SHORT).show();
+            public void onClick(View V) {
+                Intent experiment_1 = new Intent("android.intent.action.CALL");
+                experiment_1.addCategory("android.intent.category.DEFAULT");
+                startActivity(experiment_1);
             }
         });                     //此为view提供的方法
-
-
 
 
     }
