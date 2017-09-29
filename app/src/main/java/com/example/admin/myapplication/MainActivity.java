0c014bd6c9afdc60561099b6054d28cfa67a29ba
duskcloudxu <cmbjx1@qq.com>
2017-09-29 10:10:29 +0800

    9.29:添加了首页 需要优化

--- app/src/main/java/com/example/admin/myapplication/MainActivity.java
+++ app/src/main/java/com/example/admin/myapplication/MainActivity.java
@@ -32,10 +32,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btn1 = (Button) findViewById(R.id.button);//findViewById返回的是view对象，
+        Button experi_1 = (Button) findViewById(R.id.experi_1);//findViewById返回的是view对象，
         // 即在layout里出现过的对象,需要把它转型成为button对象
         //详见向下转型
-        btn1.setOnClickListener(new View.OnClickListener() {  // 这个是写在setOnClickListener底下的类
+        Button experi_2=(Button)findViewById(R.id.experi_2);
+        experi_1.setOnClickListener(new View.OnClickListener() {  // 这个是写在setOnClickListener底下的类
             @Override
             public void onClick(View V) {
                 Intent experiment_1 = new Intent("android.intent.action.CALL");
@@ -43,6 +44,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(experiment_1);
             }
         });                     //此为view提供的方法
+        experi_2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent experiment_2=new Intent(MainActivity.this, com.example.admin.myapplication.experiment_2.class);
+                startActivity(experiment_2);
+            }
+
+        });
 
 
     }
