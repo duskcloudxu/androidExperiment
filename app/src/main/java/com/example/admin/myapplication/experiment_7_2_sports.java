3f7ae898eba0e926748cd0d99176fc16c5137139
duskcloudxu <duskcloudxu@gmail.com>
2017-11-16 11:51:36 +0800

    7-3 half

--- app/src/main/java/com/example/admin/myapplication/experiment_7_2_sports.java
+++ app/src/main/java/com/example/admin/myapplication/experiment_7_2_sports.java
@@ -3,11 +3,15 @@ package com.example.admin.myapplication;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class experiment_7_2_sports extends Fragment {
     @Override
@@ -21,12 +25,23 @@ public class experiment_7_2_sports extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_experiment_7_2_sports, container, false);
-        experiment_7_2_recreation f=new experiment_7_2_recreation();
-        FragmentTransaction transaction = getChildFragmentManager().beginTransaction().add(f,"test");
+
         return view;
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        Button revokeBtn=(Button)view.findViewById(R.id.E7_2_sport_test);
+        revokeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment f = new experiment_7_2_recreation();
+                FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+                transaction.add(R.id.E7_2_sport_frame, f);
+                transaction.commit();
+            }
+        });
 
-
-
+        super.onViewCreated(view, savedInstanceState);
+    }
 }
