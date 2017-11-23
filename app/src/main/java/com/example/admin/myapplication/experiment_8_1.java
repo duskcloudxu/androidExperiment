64e17c1fe6b6416e57d5f9fa8665b9fa78050e9e
duskcloudxu <cmbjx1@qq.com>
2017-11-23 22:14:14 +0800

    11.23 8-half finished

--- app/src/main/java/com/example/admin/myapplication/experiment_8_1.java
+++ app/src/main/java/com/example/admin/myapplication/experiment_8_1.java
@@ -1,5 +1,9 @@
 package com.example.admin.myapplication;
 
+import android.annotation.TargetApi;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v4.app.NotificationCompatSideChannelService;
@@ -7,9 +11,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class experiment_8_1 extends AppCompatActivity {
+
+    private Button btn4;
+    private TextView txtView1;
+    private ProgressBar progressBar;
+    private String msg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +62,7 @@ public class experiment_8_1 extends AppCompatActivity {
                     int b = 0;
                     String s = "NULL";
                     for (int i = 0; i < 10000; i++) {
-                        for (int j = 0; j < 1000; j++) {
+                        for (int j = 0; j < 100; j++) {
                             for (int k = 0; k < 1; k++) {
                                 if (this.isInterrupted()) throw new InterruptedException("boom");
                                 b++;
@@ -73,12 +89,58 @@ public class experiment_8_1 extends AppCompatActivity {
                 t.start();
             }
         });
+
+
+        final Handler notice = new Handler() {
+            @Override
+            @TargetApi(25)
+            public void handleMessage(Message msg) {
+                ProgressBar progressBar = (ProgressBar) findViewById(R.id.E8_1_progressBar);
+                progressBar.setProgress(progressBar.getProgress() + msg.arg1, true);
+                Toast.makeText(experiment_8_1.this, "你收到了" + (String) msg.obj + "的消息" + msg.arg1, Toast.LENGTH_SHORT).show();
+                super.handleMessage(msg);
+            }
+        };
+        final Button btn1 = (Button) findViewById(R.id.E8_1_progressBtn1);
+        final progress pro1 = new progress(notice, btn1.getText().toString(), 3);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pro1.start();
+            }
+        });
+        final Button btn2 = (Button) findViewById(R.id.E8_1_progressBtn2);
+        final progress pro2 = new progress(notice, btn2.getText().toString(), 5);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pro2.start();
+            }
+        });
+        final Button btn3 = (Button) findViewById(R.id.E8_1_progressBtn3);
+        final progress pro3 = new progress(notice, btn3.getText().toString(), 7);
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pro3.start();
+            }
+        });
         final Button terminate = (Button) findViewById(R.id.E8_1_terminate);
         terminate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 t.interrupt();
+                pro1.interrupt();
+                pro2.interrupt();
+                pro3.interrupt();
             }
         });
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn4 = (Button) findViewById(R.id.E8_1_download);
+        txtView1 = (TextView) findViewById(R.id.E8_1_textVIew);
+        progressBar = (ProgressBar) findViewById(R.id.E8_1_progressBar);
+        progressBar.setVisibility(View.INVISIBLE);
     }
+
 }
---
+++ app/src/main/java/com/example/admin/myapplication/progress.java
@@ -0,0 +1,43 @@
+package com.example.admin.myapplication;
+
+import android.os.Handler;
+import android.os.Message;
+
+/**
+ * Created by duskcloud on 2017/11/23.
+ */
+
+public class progress extends Thread {
+    private String buttonName;
+    private int ascend;
+    private Handler notice;
+
+    public progress(Handler target, String btnName, int asc) {
+        ascend = asc;
+        buttonName = btnName;
+        notice = target;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                Message msg = new Message().obtain(notice);
+                msg.what = 0;
+                msg.arg1 = ascend;
+                msg.obj = buttonName;
+                msg.sendToTarget();
+                this.sleep(3000);
+                if(this.interrupted())throw new InterruptedException();
+            }
+        } catch (InterruptedException except) {
+
+        }
+    }
+
+    @Override
+    public void interrupt() {
+
+        super.interrupt();
+    }
+}
